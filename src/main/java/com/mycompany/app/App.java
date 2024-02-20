c8f4c52f61f06dc397a66aff8be4f7a94d372a4c
Too Zhi Yi <zhiyitoo@gmail.com>
2024-02-20 15:24:32 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is fantastic !");
+        System.out.println("Jenkins is fantastic, testing latest change !");
     }
 
     public String getMessage() {
