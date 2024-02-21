4c7c1ec98237dae31f71466b3e5ca057708a3847
Too Zhi Yi <zhiyitoo@gmail.com>
2024-02-21 11:02:40 +0800

    Test Webhook Trigger

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is fantastic, testing 2 latest change 21 Feb 2024 !");
+        System.out.println("Jenkins is fantastic, testing 2 latest change 21 Feb 2024 test 2 !");
     }
 
     public String getMessage() {
